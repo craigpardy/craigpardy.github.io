db3922993be7dea6cf0c38099b0e9c7dad0fc4e0
craigpardy <pardycraig@gmail.com>
2020-04-06 09:57:39 +0100

    refactored directories for images

--- scripts/first.js
+++ scripts/first.js
@@ -2,7 +2,7 @@ const express = require('express')
 var path = require('path');
 
 const app = express()
-app.use(express.static(path.join(__dirname, 'public/images')));
+app.use(express.static(path.join(__dirname, 'images')));
 const port = 80
 
 
